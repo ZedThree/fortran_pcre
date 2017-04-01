a115981e214942188def47a9cb5f684fb5823bd3
Peter Hill <zed.three@gmail.com>
2017-04-01 18:16:59 +0100

    Rudimentary workings

--- pcre_module.f90
+++ pcre_module.f90
@@ -8,16 +8,52 @@ module pcre_module
   end type pcre_type
 
   interface
+     function c_my_pcre_compile(pattern) result(regex) bind(C, name="my_pcre_compile")
+       import
+       character(len=1,kind=c_char) :: pattern(*)
+       type(c_ptr) :: regex
+     end function c_my_pcre_compile
+
+     function c_my_pcre_exec(regex, subject) result(error) bind(C, name="my_pcre_exec")
+       import
+       type(c_ptr), value :: regex
+       character(len=1,kind=c_char) :: subject(*)
+       integer(c_int) :: error
+     end function c_my_pcre_exec
+
+     function c_my_together(pattern, subject) result(error) bind(C, name="my_together")
+       import
+       character(len=1,kind=c_char) :: pattern
+       character(len=1,kind=c_char) :: subject
+       integer(c_int) :: error
+     end function c_my_together
+
+     ! pcre *pcre_compile(const char *pattern, int options, const char **errptr, int *erroffset, const unsigned char *tableptr);
      function c_pcre_compile(pattern, options, errptr, erroffset, &
           tableptr) result(pcre) bind(C, name="pcre_compile")
        import
        type(c_ptr) :: pcre
-       type(c_ptr), intent(in) :: pattern
+       character(len=1,kind=c_char), intent(in) :: pattern
        integer(c_int), intent(in), value :: options
        type(c_ptr), intent(inout) :: errptr
-       type(c_ptr), intent(out) :: erroffset
+       integer(c_int), intent(out) :: erroffset
        type(c_ptr), intent(in) :: tableptr
      end function c_pcre_compile
+
+     function c_pcre_exec(code, extra, subject, length, startoffset, &
+          options, ovector, ovecsize) &
+          result(error) bind(C, name="pcre_exec")
+       import
+       type(c_ptr), value, intent(in) :: code
+       type(c_ptr), intent(in) :: extra
+       character(len=1,kind=c_char), intent(in) :: subject
+       integer(c_int), value, intent(in) :: length
+       integer(c_int), value, intent(in) :: startoffset
+       integer(c_int), value, intent(in) :: options
+       integer(c_int), value, intent(in) :: ovecsize
+       integer(c_int), dimension(ovecsize), intent(out) :: ovector
+       integer(c_int) :: error
+     end function c_pcre_exec
 
      ! function pcre_exec() result() bind(C, name="pcre_exec")
      ! end function pcre_exec
@@ -32,29 +68,51 @@ module pcre_module
 
 contains
 
-  function pcre_compile(pattern, options) result(pcre)
-    type(pcre_type) :: pcre
+  function pcre_compile(pattern, options) result(regex)
+    implicit none
+    type(pcre_type) :: regex
 
     character(len=*), intent(in) :: pattern
     integer(c_int), intent(in) :: options
 
-    character(len=:), allocatable, target :: f_pattern
-    type(c_ptr) :: c_pattern
+    character(len=len_trim(pattern)+1, kind=c_char) :: c_pattern
     type(c_ptr) :: c_pcre
     type(c_ptr) :: error
-    type(c_ptr) :: erroffset
+    integer(c_int) :: erroffset
 
     ! NULL-terminate pattern
-    f_pattern = pattern // c_null_char
-    ! Get C address of pattern
-    c_pattern = c_loc(f_pattern)
+    c_pattern = trim(pattern) // c_null_char
 
     c_pcre = c_pcre_compile(c_pattern, options, error, erroffset, &
          c_null_ptr)
 
-    pcre%regex = c_pcre
-    ! call c_f_pointer(c_pcre, pcre%regex)
+    regex%regex = c_pcre
 
   end function pcre_compile
-  
+
+  function pcre_exec(regex, extra, subject, startoffset, &
+       options, ovector) result(error)
+    implicit none
+    type(pcre_type), intent(in) :: regex
+    type(c_ptr), intent(in) :: extra
+    character(len=*), intent(in) :: subject
+    integer, intent(in) :: startoffset
+    integer, intent(in) :: options
+    integer, dimension(:), intent(out) :: ovector
+    integer :: error
+
+    character(len=len_trim(subject)+1, kind=c_char) :: c_subject
+
+    integer, parameter :: c_ovecsize = 30
+    integer(c_int), dimension(c_ovecsize) :: c_ovector
+
+    ! NULL-terminate subject
+    c_subject = trim(subject) // c_null_char
+
+    error = c_pcre_exec(regex%regex, c_null_ptr, c_subject, &
+         len_trim(subject)+1, 0, 0, &
+         ovector, size(ovector))
+
+  end function pcre_exec
+
 end module pcre_module
--- test.f90
+++ test.f90
@@ -1,13 +1,40 @@
 program test_pcre
 
+  use, intrinsic :: iso_c_binding
   use pcre_module
 
   implicit none
 
   type(pcre_type) :: regex
+  type(c_ptr) :: c_regex
 
-  character(len=*), parameter :: pattern = "([0-9]+)"
+  character(len=*), parameter :: pattern = ".*(a..).*"
+  character(len=*), parameter :: subject = "what is this?"
+
+  integer, parameter :: ovecsize = 30
+  integer, dimension(ovecsize) :: ovector
+
+  integer :: error
+
+  ! character(len=*) :: f_pattern = pattern // c_null_char
+  character(len=len_trim(pattern)+1, kind=c_char) :: c_pattern
+  character(len=len_trim(subject)+1, kind=c_char) :: c_subject
 
   regex = pcre_compile(pattern, 0)
+
+  error = pcre_exec(regex, c_null_ptr, subject, 0, 0, ovector)
+
+  print*, ovector, error
+
+  ! NULL-terminate pattern
+  c_pattern = trim(pattern) // c_null_char
+  c_subject = trim(subject) // c_null_char
+
+  c_regex = c_my_pcre_compile(c_pattern)
+  error = c_my_pcre_exec(c_regex, c_subject)
+  print*, c_regex, error
+
+  error = c_my_together(c_pattern, c_subject)
+  print*, error
 
 end program test_pcre
