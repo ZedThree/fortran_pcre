b64eeaa0c15ff0d16373537649eec745f09003eb
Peter Hill <zed.three@gmail.com>
2017-04-06 20:36:53 +0100

    Move constants to separate module
    Add rest of constants

--- pcre_module.f90
+++ pcre_module.f90
@@ -3,54 +3,23 @@ module pcre_module
   use, intrinsic :: iso_c_binding
   use, intrinsic :: iso_fortran_env
 
+  use pcre_constants
+
   implicit none
-
-  ! Possible errors
-  integer, parameter :: PCRE_ERROR_NOMATCH          = -1
-  integer, parameter :: PCRE_ERROR_NULL             = -2
-  integer, parameter :: PCRE_ERROR_BADOPTION        = -3
-  integer, parameter :: PCRE_ERROR_BADMAGIC         = -4
-  integer, parameter :: PCRE_ERROR_UNKNOWN_OPCODE   = -5
-
-  ! Request types for pcre_fullinfo
-  integer, parameter :: PCRE_INFO_OPTIONS             = 0
-  integer, parameter :: PCRE_INFO_SIZE                = 1
-  integer, parameter :: PCRE_INFO_CAPTURECOUNT        = 2
-  integer, parameter :: PCRE_INFO_BACKREFMAX          = 3
-  integer, parameter :: PCRE_INFO_FIRSTBYTE           = 4
-  integer, parameter :: PCRE_INFO_FIRSTTABLE          = 5
-  integer, parameter :: PCRE_INFO_LASTLITERAL         = 6
-  integer, parameter :: PCRE_INFO_NAMEENTRYSIZE       = 7
-  integer, parameter :: PCRE_INFO_NAMECOUNT           = 8
-  integer, parameter :: PCRE_INFO_NAMETABLE           = 9
-  integer, parameter :: PCRE_INFO_STUDYSIZE           = 10
-  integer, parameter :: PCRE_INFO_DEFAULT_TABLES      = 11
-  integer, parameter :: PCRE_INFO_OKPARTIAL           = 12
-  integer, parameter :: PCRE_INFO_JCHANGED            = 13
-  integer, parameter :: PCRE_INFO_HASCRORLF           = 14
-  integer, parameter :: PCRE_INFO_MINLENGTH           = 15
-  integer, parameter :: PCRE_INFO_JIT                 = 16
-  integer, parameter :: PCRE_INFO_JITSIZE             = 17
-  integer, parameter :: PCRE_INFO_MAXLOOKBEHIND       = 18
-  integer, parameter :: PCRE_INFO_FIRSTCHARACTER      = 19
-  integer, parameter :: PCRE_INFO_FIRSTCHARACTERFLAGS = 20
-  integer, parameter :: PCRE_INFO_REQUIREDCHAR        = 21
-  integer, parameter :: PCRE_INFO_REQUIREDCHARFLAGS   = 22
-  integer, parameter :: PCRE_INFO_MATCHLIMIT          = 23
-  integer, parameter :: PCRE_INFO_RECURSIONLIMIT      = 24
-  integer, parameter :: PCRE_INFO_MATCH_EMPTY         = 25
 
   type pcre_type
      type(c_ptr) :: regex
   end type pcre_type
 
   interface
-     ! pcre *pcre_compile(const char *pattern, int options, const char **errptr, int *erroffset, const unsigned char *tableptr);
+     ! pcre *pcre_compile(const char *pattern, int options,
+     !                    const char **errptr, int *erroffset,
+     !                    const unsigned char *tableptr);
      function c_pcre_compile(pattern, options, errptr, erroffset, &
           tableptr) result(pcre) bind(C, name="pcre_compile")
        import
        type(c_ptr) :: pcre
-       character(len=1,kind=c_char), intent(in) :: pattern
+       character(len=1, kind=c_char), intent(in) :: pattern(*)
        integer(c_int), intent(in), value :: options
        type(c_ptr), intent(inout) :: errptr
        integer(c_int), intent(out) :: erroffset
@@ -65,7 +34,7 @@ module pcre_module
        import
        type(c_ptr), value, intent(in) :: code
        type(c_ptr), value, intent(in) :: extra
-       character(len=1,kind=c_char), intent(in) :: subject
+       character(len=1,kind=c_char), intent(in) :: subject(*)
        integer(c_int), value, intent(in) :: length
        integer(c_int), value, intent(in) :: startoffset
        integer(c_int), value, intent(in) :: options
